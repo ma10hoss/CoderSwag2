24391c57928af017eb3f52a42237e48de39b3647
Arman Hossain <ma10hoss@siena.edu>
2018-11-09 12:54:33 -0500

    created a costuem category list item in xml

--- app/src/main/java/com/example/coding/Controllers/MainActivity.kt
+++ app/src/main/java/com/example/coding/Controllers/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.coding.Controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.coding.Models.Category
 import com.example.coding.R
+import com.example.coding.Services.DataServices
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataServices.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/coding/Models/Category.kt
+++ app/src/main/java/com/example/coding/Models/Category.kt
@@ -1,4 +1,8 @@
 package com.example.coding.Models
 
 class Category(val title: String, val image: String) {
+    // over rides the default for the contents inside array adapters. puts in the title:string
+    override fun toString(): String {
+        return title
+    }
 }

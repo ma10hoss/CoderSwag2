4b229a7eb3501319f5a5ea40fb1b223c479d06bd
Arman Hossain <ma10hoss@siena.edu>
2018-11-12 11:47:18 -0500

    added a get products function with a return when value

--- app/src/main/java/com/example/coding/Services/DataServices.kt
+++ app/src/main/java/com/example/coding/Services/DataServices.kt
@@ -6,26 +6,26 @@ import com.example.coding.Models.Product
 object DataServices {
     val categories = listOf(
         Category("SHIRTS", "shirtimage"),
-        Category("HOODIES","hoodieimages"),
-        Category("HATS","hatimages"),
-        Category("DIGITAL","digitalgoodimage"),
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
+        Category("DIGITAL","digitalgoodsimage"),
         Category("SHIRTS", "shirtimage"),
-        Category("HOODIES","hoodieimages"),
-        Category("HATS","hatimages"),
-        Category("DIGITAL","digitalgoodimage"),
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
+        Category("DIGITAL","digitalgoodsimage"),
         Category("SHIRTS", "shirtimage"),
-        Category("HOODIES","hoodieimages"),
-        Category("HATS","hatimages"),
-        Category("DIGITAL","digitalgoodimage")
+        Category("HOODIES","hoodieimage"),
+        Category("HATS","hatimage"),
+        Category("DIGITAL","digitalgoodsimage")
     )
     val hats = listOf(
         Product("Devslopes Grafic Beanie","$18","hat1"),
         Product("Devslopes Hat Black","$20","hat2"),
-        Product("Devslopes Hat White","$22","hat0"),
+        Product("Devslopes Hat White","$22","hat3"),
         Product("Devslopes Hat Snapback","$24","hat4"),
         Product("Devslopes Grafic Beanie","$18","hat1"),
         Product("Devslopes Hat Black","$20","hat2"),
-        Product("Devslopes Hat White","$22","hat0"),
+        Product("Devslopes Hat White","$22","hat3"),
         Product("Devslopes Hat Snapback","$24","hat4")
     )
     val hoodies = listOf(
@@ -48,4 +48,15 @@ object DataServices {
         Product("Devslopes Hoodie hustle","$20","shirt4"),
         Product("KickFlip Studio","$18","shirts5")
     )
+    val digitalGoods = listOf<Product>()
+
+    fun getProducts(category: String) : List<Product>{
+
+        return when (category) {
+            "shirts" -> shirts
+            "HATS" -> hats
+            "Hoodies" -> hoodies
+            else -> digitalGoods
+        }
+    }
 }
